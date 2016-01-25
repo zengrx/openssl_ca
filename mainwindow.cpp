3804cd17ee3b98c445feaada5927daa1caa762e7
QoolAtGit <xiaomi2aplay@gmail.com>
2016-01-25 16:10:02 +0800

    add features and bugs fix

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -31,33 +31,49 @@ void MainWindow::on_pushButton_clicked()
 void MainWindow::on_pushButton_7_clicked()
 {
     verify.userCerUrl = QFileDialog::getOpenFileName(this,"select file","./",NULL);
-    Load_Cer();
-    message += getTime() + "rootCert loaded ...\n";
-    message += noTime() + "userCert loaded ...\n";
-    message += noTime() + "PrivateKey loaded ...\n";
-    showMessage();
-    certInfo info;
-    QString tmpstr_0=GetCertSubjectString(&info);
-    if(!tmpstr_0.isNull())
+    if (verify.userCerUrl.isNull())
     {
-        ui->lineEdit_22->setText(info.client);
-        ui->lineEdit_23->setText(info.country);
-        ui->lineEdit_24->setText(info.state);
-        ui->lineEdit_25->setText(info.location);
-        ui->lineEdit_26->setText(info.organization);
-        ui->lineEdit_27->setText(info.organizationalUnitName);
-        ui->lineEdit_28->setText(info.email);
-        message += getTime() + "Certificate Detail:";
-        QString tmpstr_1 = GetCertSerialNumber();
-        if(!tmpstr_1.isNull())
-        {
-            message += "\n" + noTime() + "SerialNumber: ";
-            message += tmpstr_1;
-        }
-        message += tmpstr_0;
-        message += "\n";
+        QMessageBox::information(NULL,"error","Select file failed!\n");
+        message += getTime() + "Select file failed!\n";
+        showMessage();
     }
-    showMessage();
+    else
+    {
+        if(Load_Cer()>0)
+        {
+            message += getTime() + "rootCert loaded ...\n";
+            message += noTime() + "userCert loaded ...\n";
+            message += noTime() + "PrivateKey loaded ...\n";
+            showMessage();
+            certInfo info;
+            QString tmpstr_0=GetCertSubjectString(&info);
+            if(!tmpstr_0.isNull())
+            {
+                ui->lineEdit_22->setText(info.client);
+                ui->lineEdit_23->setText(info.country);
+                ui->lineEdit_24->setText(info.state);
+                ui->lineEdit_25->setText(info.location);
+                ui->lineEdit_26->setText(info.organization);
+                ui->lineEdit_27->setText(info.organizationalUnitName);
+                ui->lineEdit_28->setText(info.email);
+                message += getTime() + "Certificate Detail:";
+                QString tmpstr_1 = GetCertSerialNumber();
+                if(!tmpstr_1.isNull())
+                {
+                    message += "\n" + noTime() + "SerialNumber: ";
+                    message += tmpstr_1;
+                }
+                message += tmpstr_0;
+                message += "\n";
+            }
+            showMessage();
+        }
+        else
+        {
+            message += getTime()+"Load file faild!\n";
+            showMessage();
+        }
+    }
 }
 
 //验证证书
@@ -146,4 +162,73 @@ QString MainWindow::getTime()
 QString MainWindow::noTime()
 {
     return "[        ]  ";
+}
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    X509 *rootCert = NULL;          //根证书
+    EVP_PKEY *pkey;
+    BIO *b;                         //接收证书等待格式化
+    b=BIO_new_file("rootca1.crt","r");
+    if(b==NULL)
+    {
+        QMessageBox::information(NULL,"Error","Load rootca1.crt failed!\n");
+        message += getTime() + "Load CA(rootca1.crt) failed! Please make sure file exist.\n";
+        showMessage();
+        BIO_free(b);
+        return;
+    }
+    else
+    {
+        rootCert = PEM_read_bio_X509(b,NULL,NULL,NULL);
+        b=NULL;
+    }
+    verify.rootCert=rootCert;
+    b = BIO_new_file("rootca1.key", "r");
+    if(b==NULL)
+    {
+        QMessageBox::information(NULL,"Error","Load CRL.crl failed!\n");
+        message += getTime() + "Load CRL.crl failed! Please make sure file exist.\n";
+        showMessage();
+        BIO_free(b);
+        return;
+    }
+    else
+    {
+        pkey=PEM_read_bio_PrivateKey(b,NULL,NULL,NULL);
+        b=NULL;
+    }
+    verify.pkey=pkey;
+    if(Crl()>0)
+    {
+        QMessageBox::information(NULL,"Sucess","Create CRL Sucess!\n");
+        message += getTime() + "Create CRL sucessed!\n";
+        showMessage();
+    }
+    else
+    {
+        QMessageBox::information(NULL,"Fail","Create CRL Failed!\n");
+        message += getTime() + "Create CRL failed!\n";
+        showMessage();
+    }
+}
+
+void MainWindow::on_pushButton_4_clicked()
+{
+    /*
+    CreateCrl();*/
+    if(Revoked_Load_Cer()<=0)
+    {
+        QMessageBox::information(this,"Error","Revoked_Load_Cer failed","确定");
+        return;
+    }
+    QString strtmp=ui->lineEdit_9->text();
+    verify.ser=strtmp;
+    if(strtmp==NULL)
+        QMessageBox::warning(this,"警告","请输入证书序列号！","确定");
+    else
+        if(revokedCert())
+            QMessageBox::information(this,"提示","撤销成功！","确定");
+        else
+            QMessageBox::information(this,"提示","撤销失败！","确定");
 }
